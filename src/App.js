241afb6e7b9c3eb55bc017104f5359a7eed58f8f
SoeLaPyaeHtun <soelapyaehtun@gmail.com>
2023-08-27 19:41:24 +0800

    ch path3

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<NavBarL />}>
           <Route path="/" element={<First />} />
-          <Route path="/*" element={<Navigate to="/" />} />
+          <Route path="*" element={<Navigate to="/" />} />
           <Route path="/hire" element={<Hire />} />
           <Route path="/s" element={<SoeLaPyaeHtun />} />
         
